7e213acba4f140d9063eeea328b0cf0206127cdb
Joshua <marichaljoshua@gmail.com>
2021-03-05 04:24:31 +0000

    DOM 2da parte

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,15 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+	for (let i = 0; i < countries.length; i++) {
+		let select = document.createElement("option");
+		select.innerHTML = countries[i];
+		let lista = document.querySelector("#mySelect");
+		lista.appendChild(select);
+	}
 };
+let aux = document.querySelector("#mySelect");
+aux.addEventListener("change", function() {
+	alert(aux.value);
+});
