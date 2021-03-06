db04d4f9ca6be6476ec6f086546fc6af7b9e583c
Joshua <marichaljoshua@gmail.com>
2021-03-06 02:21:12 +0000

    Ejercicio DOM

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,15 @@
 // Your code here
+let add = document.querySelector("#addToDo");
+add.addEventListener("keyup", function(e) {
+	if (e.keyCode === 13) {
+		var tarea = document.createElement("li");
+		tarea.innerHTML = `<span><i class="fa fa-trash"></i>${add.value}</span>`;
+		var myList = document.querySelector("ul");
+		myList.appendChild(tarea);
+
+		tarea.addEventListener("click", function() {
+			let todos = document.querySelectorAll("ul");
+			todos[0].removeChild(todos);
+		});
+	}
+});
